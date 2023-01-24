2097d456ca8dbef6beed4ceef7c6c4afa6d8fb26
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2023-01-24 17:16:58 +0200

    1-task-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  intervalId = setInterval(() => {
+    bodyEl.style.background = getRandomHexColor();
+  }, 1000);
+    
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+};
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+        
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+
+
 
